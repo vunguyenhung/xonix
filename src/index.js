e81ef1391a54501eeb15728da5fdb023331b2ce6
vunguyenhung <vunguyenhung@outlook.com>
2017-12-01 03:12:56 +0700

    change Game State -> World State, add get Game State command

--- src/index.js
+++ src/index.js
@@ -23,13 +23,13 @@ const play = () => {
 		output: process.stdout,
 		prompt: 'Xonix> ',
 	});
-	console.log('>>Initiating Game States...');
+	console.log('>>Initiating World States...');
 	store.dispatch(InitiateStatesAction());
-	console.log('>>Game States initiated, use `m` command to show Game States');
+	console.log('>>World States initiated, use `m` command to show World States');
 
 	console.log('Xonix available commands:');
 	console.log('-------------------------');
-	console.log('init: Init Game states');
+	console.log('init: Init World states');
 	console.log('n: Steer the car to North');
 	console.log('s: Steer the car to South');
 	console.log('e: Steer the car to East');
@@ -37,7 +37,8 @@ const play = () => {
 	console.log('i: Increase Car speed by one unit');
 	console.log('l: Decrease Car speed by one unit');
 	console.log('b: Add new Monster Ball to the world');
-	console.log('m: Show Game States');
+	console.log('d: Show Game States');
+	console.log('m: Show World States');
 	console.log('q: Quit the game');
 	console.log('hello: Print `world`');
 	rl.prompt();
@@ -47,63 +48,69 @@ const play = () => {
 				console.log('world!');
 				break;
 			case 'init':
-				console.log('Init Game States');
+				console.log('Init World States');
 				store.dispatch(InitiateStatesAction());
 				console.log(store.getState());
 				break;
 			case 'n':
 				console.log('Steer Car to North');
 				console.log('-------------------------');
-				console.log('GameStates.car: ');
+				console.log('WorldStates.car: ');
 				store.dispatch(SteerCarAction(0));
 				console.log(prop('car')(store.getState()));
 				break;
 			case 's':
 				console.log('Steer Car to South');
 				console.log('-------------------------');
-				console.log('GameStates.car: ');
+				console.log('WorldStates.car: ');
 				store.dispatch(SteerCarAction(180));
 				console.log(prop('car')(store.getState()));
 				break;
 			case 'e':
 				console.log('Steer Car to East');
 				console.log('-------------------------');
-				console.log('GameStates.car: ');
+				console.log('WorldStates.car: ');
 				store.dispatch(SteerCarAction(90));
 				console.log(prop('car')(store.getState()));
 				break;
 			case 'w':
 				console.log('Steer Car to West');
 				console.log('-------------------------');
-				console.log('GameStates.car: ');
+				console.log('WorldStates.car: ');
 				store.dispatch(SteerCarAction(270));
 				console.log(prop('car')(store.getState()));
 				break;
 			case 'i':
 				console.log('Increase Car speed by one unit');
 				console.log('-------------------------');
-				console.log('GameStates.car: ');
+				console.log('WorldStates.car: ');
 				store.dispatch(IncreaseCarSpeedAction());
 				console.log(prop('car')(store.getState()));
 				break;
 			case 'l':
 				console.log('Decrease Car speed by one unit');
 				console.log('-------------------------');
-				console.log('GameStates.car: ');
+				console.log('WorldStates.car: ');
 				store.dispatch(DecreaseCarSpeedAction());
 				console.log(prop('car')(store.getState()));
 				break;
 			case 'b':
 				console.log('Add new Monster Ball to the world');
 				console.log('-------------------------');
-				console.log('GameStates.monsterBalls: ');
+				console.log('WorldStates.monsterBalls: ');
 				store.dispatch(AddMonsterBallAction());
 				console.log(prop('monsterBalls')(store.getState()));
 				break;
-			case 'm':
-				console.log('Show Game States');
+			case 'd':
+				console.log('Show World States');
 				console.log('-------------------------');
-				console.log('GameStates: ');
+				console.log('WorldStates.game: ');
+				console.log(prop('game')(store.getState()));
+				break;
+			case 'm':
+				console.log('Show World States');
+				console.log('-------------------------');
+				console.log('WorldStates: ');
 				console.log(store.getState());
 				break;
 			case 'q':
